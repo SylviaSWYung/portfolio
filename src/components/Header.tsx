1d731e9b3d1a31666b890c9a05f195e9d1ef3af4
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-03-08 20:17:40 +0100

    Implemented header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,33 @@
 export const Header = () => {
-    return (
-        <div>
-         <p className='font-prata'>her er det en ehader</p>
-        </div>
-    )
-}
+  return (
+    <div className="h-15 bg-header border-b-1 border-headerborder flex items-center justify-between">
+      <h1 className="ml-6 font-prata text-text-dark text-2xl ">
+        Sylvia Suet Wai Yung
+      </h1>
+      <button type="button" className="font-prata text-lg text-text-dark border-1 rounded-2xl h-8 w-22 bg-button/50 drop-shadow-lg ml-8">
+        RESUME
+      </button>
+      <button
+            // onClick={toggleMenu}
+            type="button"
+            className="items-center mr-4 h-10 justify-center rounded-lg md:hidden"
+            // aria-expanded={isExpanded}
+          >
+            <svg
+              className="h-5"
+            //   aria-hidden="true"
+            //   fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="#b66878"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+    </div>
+  );
+};
