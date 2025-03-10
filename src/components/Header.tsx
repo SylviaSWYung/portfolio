73130d5240fdbaee9103107152a59f80ff3dff4a
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-03-10 19:55:55 +0100

    Change responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,27 +13,32 @@ export const Header = () => {
   };
 
   return (
-    <div className="h-15 bg-header border-b-1 border-headerborder flex items-center justify-between">
-      <h1 className="ml-6 font-inria text-text-dark text-2xl ">
+    <div className="h-15 sm:h-18 bg-header border-b-1 border-headerborder flex items-center justify-between">
+      <h1 className="ml-6 sm:ml-20 font-inria text-text-dark text-2xl sm:text-4xl ">
         Sylvia Suet Wai Yung
       </h1>
+      <div className="hidden sm:flex font-inria text-3xl text-text-dark gap-16 hover:text-gray-900">
+        <a href="/about">ABOUT ME</a>
+        <a href="/projects">PROJECT</a>
+        <a href="/skills">SKILLS</a>
+      </div>
       <button
         type="button"
         onClick={resume}
-        className="font-inria text-lg text-text-dark border-1 rounded-2xl h-8 w-22 bg-button/50 shadow-lg ml-8 hover:bg-hover/50"
+        className="font-inria text-lg sm:mr-20 sm:text-2xl text-text-dark border-1 rounded-2xl h-8 w-22 sm:h-10 sm:w-28 bg-button/50 shadow-lg ml-8 hover:bg-hover/50"
       >
         RESUME
       </button>
       <button
         onClick={toggleMenu}
         type="button"
-        className="items-center mr-4 h-10 justify-center rounded-lg md:hidden"
+        className="items-center mr-4 h-10 justify-center rounded-lg sm:hidden"
       >
         <HamburgerMenu />
       </button>
-      {/* Dropdown menu */}
+      {/* Mobile Dropdown menu */}
       {isExpanded && (
-        <div className="flex flex-col items-center z-10 absolute w-full top-15 bg-header ${isExpanded ? 'block' : 'hidden'} p-8 font-inria ">
+        <div className="flex flex-col items-center z-10 absolute w-full top-15 bg-header sm:hidden p-8 font-inria ">
           <a href="/about" className="block text-text-dark hover:text-gray-900">
             ABOUT ME
           </a>
