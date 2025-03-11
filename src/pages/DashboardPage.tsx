63d96f62b329b66600310cf6f9d61e5ef21700db
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-03-11 10:20:59 +0100

    Finish DashboardPage for PC and mobile

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,10 +4,11 @@ import { AnimatedText } from "../components/AnimatedText";
 export const DashboardPage = () => {
   return (
     //Mulig fjerne flex-wrap?
-    <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-evenly sm:items-center sm:gap-x-4 sm:max-w-full overflow-hidden min-h-screen">
+    <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-evenly sm:items-center sm:gap-x-4 sm:max-w-full sm:overflow-hidden sm:min-h-screen">
       <div className="relative flex justify-center sm:order-2">
+        {/*På høyre side av bildet*/}
         <motion.img
-          className="absolute left-2/5 sm:left-9/12 -rotate-10 scale-20 sm:scale-30 sm:bottom-2/3"
+          className="absolute left-2/5 sm:left-9/12 -rotate-10 sm:rotate-10 scale-20 sm:scale-30 sm:bottom-2/3"
           src="bow.png"
           alt=""
           animate={{ y: [0, -20, 0] }}
@@ -24,7 +25,7 @@ export const DashboardPage = () => {
         />
       </div>
 
-      {/*Text and button */}
+      {/*Text og button */}
       <div className="flex flex-col ml-8 mr-8 sm:mr-0 sm:ml-0 sm:order-1 sm:w-full sm:max-w-[800px]">
         <p className="font-inria text-text-light text-4xl sm:text-7xl sm:mb-5">
           Hi, My name is
@@ -70,8 +71,33 @@ export const DashboardPage = () => {
           </button>
         </div>
       </div>
+      {/*I mobil versjon - under buttons. I computer version - over Hi, my name */}
       <motion.img
-        className="relative sm:absolute sm:left-1/20 sm:size-60 sm:bottom-2/3 sm:rotate-25 left-10 mt-5 rotate-12 size-1/3"
+        className="relative sm:absolute sm:left-1/20 sm:size-50 sm:bottom-2/3 sm:rotate-25 left-10 mt-5 rotate-12 size-1/3"
+        src="bow.png"
+        alt=""
+        animate={{ y: [0, -20, 0] }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      />
+      {/*Hidden i mobil versjon, men venstre side av bildet */}
+      <motion.img
+        className="hidden sm:absolute sm:block sm:right-1/3 sm:top-1/6 sm:-rotate-30 sm:scale-40"
+        src="bow.png"
+        alt=""
+        animate={{ y: [0, -20, 0] }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      />
+      {/*Hidden i mobil versjon, men nedre venstre side av teksten */}
+      <motion.img
+        className="hidden sm:absolute sm:block sm:right-3/4 sm:top-3/5 sm:-rotate-30 sm:scale-40"
         src="bow.png"
         alt=""
         animate={{ y: [0, -20, 0] }}
