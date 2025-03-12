403fce4985b487ca726390ac714afbafc2f86f0a
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-03-12 17:50:33 +0100

    Changed resolution

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,23 +1,23 @@
 import { motion } from "framer-motion";
 
 const buttonInText =
-  "text-lg sm:text-2xl xl:text-3xl text-text-dark border-1 rounded-full h-10 w-26 sm:h-12 xl:h-16 sm:w-33 xl:w-44 bg-button";
+  "text-base sm:text-2xl xl:text-3xl text-text-dark border-1 rounded-full h-8 w-22 sm:h-12 xl:h-16 sm:w-33 xl:w-44 bg-button";
 const titleProject =
-  "border-t-1 border-headerborder flex items-center justify-between h-18";
+  "border-t-1 border-headerborder flex items-center justify-between h-16";
 
 export const ProjectPage = () => {
-  // const [projectPicture, setProjectPicture] = useState("");
+  // const [projectPicture, setProjectPicture] = useState<string | null>(null);
 
   return (
     <div className="flex flex-col min-h-screen items-center">
-      <h1 className="font-inria text-headerborder text-4xl mt-10 mb-5">
+      <h1 className="font-inria text-headerborder text-3xl sm:text-4xl mt-10 mb-5">
         University Projects
       </h1>
       {/*Boks for bilde */}
-      <div className="border-4 text-headerborder bg-button/50 rounded-4xl p-4 m-4 h-60 w-60">
+      <div className="border-4 text-headerborder bg-button/50 rounded-4xl p-4 m-4 min-h-50 min-w-50">
         <p>Bilde her</p>
       </div>
-      <div className="flex-col w-sm overflow-hidden font-inria text-text-dark text-3xl">
+      <div className="flex-col w-4/5 overflow-hidden font-inria text-text-dark text-xl sm:text-3xl">
         <motion.img
           className="size-1/12"
           src="chicken.gif"
