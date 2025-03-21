5654c77beaed02ade59fb76de45b639a816631d7
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-03-21 13:17:04 +0100

    updated homepage after vercel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,8 +7,8 @@ const buttonStyle =
 export const HomePage = () => {
   return (
     //Mulig fjerne flex-wrap?
-    <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-evenly sm:items-center sm:gap-x-4 sm:max-w-full sm:h-[calc(100vh-4.5rem)]">
-      <div className="relative flex justify-center sm:order-2">
+    <div className="flex flex-col md:flex-row md:flex-wrap sm:justify-evenly sm:items-center sm:gap-x-4 sm:max-w-full sm:h-[calc(100vh-4.5rem)]">
+      <div className="relative flex justify-center md:order-2">
         {/*På høyre side av bildet*/}
         <motion.img
           className="absolute sm:hidden xl:block left-9/12 top-3/4 sm:left-11/12 -rotate-10 sm:rotate-10 max-w-16 sm:max-w-1/5 sm:top-3/4"
@@ -22,7 +22,7 @@ export const HomePage = () => {
           }}
         />
         <img
-          className="rounded-full mx-auto my-12 w-full max-w-[200px] sm:max-w-[350px] lg:max-w-[350px]"
+          className="rounded-full mx-auto my-12 w-full max-w-[200px] sm:max-w-[300px] lg:max-w-[350px]"
           src="/Sylvia.jpg"
           alt="Picture of Sylvia"
         />
@@ -76,7 +76,7 @@ export const HomePage = () => {
       </div>
       {/*I mobil versjon - under buttons. I computer version - over Hi, my name */}
       <motion.img
-        className="relative left-10 mt-5 rotate-12 w-1/3 sm:w-1/15 sm:absolute lg:block sm:left-1/12 sm:top-1/9 sm:rotate-25 "
+        className="relative left-10 mt-5 rotate-12 w-1/3 sm:w-1/15 sm:absolute sm:hidden lg:block sm:left-1/12 sm:top-1/9 sm:rotate-25 "
         src="bow.png"
         alt=""
         animate={{ y: [0, -10, 0] }}
@@ -100,7 +100,7 @@ export const HomePage = () => {
       />
       {/*Hidden i mobil versjon, men nedre venstre side av teksten */}
       <motion.img
-        className="hidden sm:absolute sm:w-1/4 sm:block sm:right-3/8 lg:block lg:right-3/4 sm:top-7/12 sm:-rotate-30 sm:scale-40"
+        className="hidden sm:absolute sm:w-1/4 sm:right-3/8 sm:hidden lg:block lg:right-3/4 sm:top-7/12 sm:-rotate-30 sm:scale-40"
         src="bow.png"
         alt=""
         animate={{ y: [0, -10, 0] }}
