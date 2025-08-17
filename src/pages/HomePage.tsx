71f65ddde29794680ca4f11c916b0ea8656e7161
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-08-17 17:26:59 +0200

    update homepage picture

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,8 +12,8 @@ export const HomePage = () => {
       <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-evenly sm:items-center sm:gap-x-4 sm:max-w-full sm:min-h-[calc(100vh-10rem)]">
         <div className="relative flex justify-center sm:order-1 md:order-2">
           <img
-            className="rounded-full mx-auto my-12 max-w-1/2 sm:max-w-sm"
-            src="homepage/Sylvia2.jpg"
+            className="rounded-full mx-auto sm:my-12 max-w-1/2 sm:max-w-sm"
+            src="homepage/Sylvia3.png"
             alt="Picture of Sylvia"
           />
         </div>
